88440fb6447b2274585421a583ed0efda039e669
alimmv <163839135+alimmv@users.noreply.github.com>
2025-09-19 18:11:36 +0600

    Update CoinAcceptor.java
    Refactor CoinAcceptor to implement MoneyAcceptor

--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -1,14 +1,25 @@
 package model;
 
-public class CoinAcceptor {
+public class CoinAcceptor implements MoneyAcceptor {
     private int amount;
 
     public CoinAcceptor(int amount) {
         this.amount = amount;
     }
 
+    @Override
     public int getAmount() {
         return amount;
+    }
+
+    @Override
+    public void addMoney(int amount) {
+        this.amount += amount;
+    }
+
+    @Override
+    public void deductMoney(int amount) {
+        this.amount -= amount;
     }
 
     public void setAmount(int amount) {
